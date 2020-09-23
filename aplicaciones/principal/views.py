eefeef4e96893197f85db4ba192eff72dfc40e60
antonio <marcoantonio_martinez@outlook.com>
2020-09-23 05:54:39 -0500

    creacion de contacto

---
+++ aplicaciones/principal/views.py
@@ -0,0 +1,28 @@
+from django.shortcuts import render ,redirect
+from .models import Persona
+from .forms import PersonaForm
+
+# Create your views here.
+def inicio(request):
+    personas = Persona.objects.all()
+    contexto = {
+        'personas':personas
+    }
+    return render(request, 'index.html',contexto)
+
+def crearPersona(request):
+    if request.method == 'GET':
+        form = PersonaForm()
+        contexto = {
+            'form':form
+         }
+    else:
+        form = PersonaForm(request.POST)
+        contexto = {
+            'form':form
+        }
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    return render(request, 'crear_persona.html',contexto)
+
